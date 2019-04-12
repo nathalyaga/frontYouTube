2ff30fedf7c22f7f7ba2b61e0d848f5e9b8aaded
unknown <nathalyaga@gmail.com>
2019-04-12 13:52:25 -0500

    USer lis created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
